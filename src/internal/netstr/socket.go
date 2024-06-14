1cef9f72f2a2c3e0ed6ce9b31e5defd8e8c34ab1
angrykirc <kirillmurz@yandex.ru>
2024-06-14 12:35:21 +0300

    lol fix typo

--- src/internal/netstr/socket.go
+++ src/internal/netstr/socket.go
@@ -54,20 +54,21 @@ func listen(log *log.Logger, addr netip.AddrPort) (net.PacketConn, error) {
 	return l, nil
 }
 
-func readFrom(debug bool, log *log.Logger, pc net.PacketConn) ([]byte, netip.AddrPort, error) {
+func readFrom(debug bool, pc net.PacketConn) ([]byte, netip.AddrPort, error) {
 	const buflen = 65535
 	buf := make([]byte, buflen)
 
 	n, src, err := pc.ReadFrom(buf)
+	buf = buf[:n]
 	ap := GetAddr(src)
 	if err != nil {
 		Log.Println(err)
 		return buf, ap, err
 	}
 	if debug {
-		log.Printf("recv %s -> %s [%d]\n%x", ap, pc.LocalAddr(), n, buf[:n])
+		log.Printf("recv %s -> %s [%d]\n%x", ap, pc.LocalAddr(), n, buf)
 	}
-	return buf[:n], ap, nil
+	return buf, ap, nil
 }
 
 func WriteTo(pc net.PacketConn, buf []byte, addr netip.AddrPort) (int, error) {
